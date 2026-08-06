6763b19f77802e1bc072b970fa14df07848beb0a
huydd-ai <huydd@pixon.games>
2026-08-06 09:57:47 +0700

    refactor(errors): add index.ts barrel export for error page components

---
+++ frontend/src/components/errors/index.ts
@@ -0,0 +1,7 @@
+export * from "./BadRequestPage";
+export * from "./ErrorBoundary";
+export * from "./ForbiddenPage";
+export * from "./NotFoundPage";
+export * from "./ServiceUnavailablePage";
+export * from "./StatusErrorPage";
+export * from "./UnauthorizedPage";
